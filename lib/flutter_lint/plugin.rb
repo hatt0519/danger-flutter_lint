56070bbd79e4caa7888f0104f59d1c4680c64dd0
hatt0519 <moroku0519@gmail.com>
2024-07-17 05:30:28 +0900

    add selector with all labels

--- lib/flutter_lint/plugin.rb
+++ lib/flutter_lint/plugin.rb
@@ -87,6 +87,7 @@ module Danger
         return [] if filtered_input.detect { |element| element.include? "No issues found!" }
 
         filtered_input
+          .select { |line| line.start_with? ("info", "warning", "error") }
           .map(&method(:parse_line))
       end
 
